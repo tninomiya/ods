54f31613a4ce9dce276f598bdac11accfe4b0904
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-11-18 18:33:19 +0900

    [SEC-01] add interfaces

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![warn(missing_docs)]
+//! interface represents abstract data type which defines whole operations and its semantics for a data structure.
+
+/// Provide list interface
+pub mod list;
+/// Provide queue related interface
+pub mod queue;
+/// Provide set releated interface
+pub mod set;
---
+++ interface/src/queue.rs
@@ -0,0 +1,37 @@
+/// Queue represents a collection of elements.
+/// Provides manipulations for addition, removal with a certain rule.
+pub trait Queue<T>
+where
+    T: std::fmt::Debug + Clone,
+{
+    /// Add a value to a queue.
+    fn add(&mut self, x: T);
+    /// Remove a next value and return it.
+    fn remove(&mut self) -> Option<T>;
+}
+
+/// Stack represents LIFO queue.
+pub trait Stack<T>
+where
+    T: std::fmt::Debug + Clone,
+{
+    /// Add a value to the tail of a queue.
+    fn push(&mut self, x: T);
+    /// Remove a last-added value.
+    fn pop(&mut self) -> Option<T>;
+}
+
+/// Dequeue represents double-ended queue.
+pub trait Deque<T>
+where
+    T: std::fmt::Debug + Clone,
+{
+    /// Add a value to the head of a queue.
+    fn add_first(&mut self, x: T);
+    /// Remove a value from the head of a queue.
+    fn remove_first(&mut self) -> Option<T>;
+    /// Add a value to the tail of a queue.
+    fn add_last(&mut self, x: T);
+    /// Remove a value from the tail of a queue.
+    fn remove_last(&mut self) -> Option<T>;
+}
