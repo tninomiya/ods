1f31bbf9da63b2d848cea63b8d3f3f6051b74b72
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-12-13 12:32:39 +0900

    [SEC-02] prepare skeleton

--- array_list/src/lib.rs
+++ array_list/src/lib.rs
@@ -1,7 +1,9 @@
 #![warn(missing_docs)]
 //! array_list implements List and Queue interfaces with backing array.
 
+/// Implementation for List optimized to realize efficient addition/removal from .
+pub mod array_deque;
 /// Implementation for List optimized to realize FIFO queue.
 pub mod array_queue;
-/// Implementation for List optimized to realize Stack.
+/// Implementation for List optimized to realize double-ended queue.
 pub mod array_stack;
